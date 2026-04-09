047f368859e4f233476e11160f7518f49c7efeec
sjk100 <47692599+sjk100@users.noreply.github.com>
2026-04-09 23:17:43 +0100

    edit path
    Updated file paths in SQL copy commands to correct locations.

--- 01_structure/load.sql
+++ 01_structure/load.sql
@@ -1,5 +1,5 @@
-\copy raw_staging_games FROM 'path:/coding_projects/video_game_sales/vgsales.csv' DELIMITER ',' CSV HEADER NULL 'N/A';
-\copy staging_games FROM 'path:/coding_projects/video_game_sales/vgsales.csv' DELIMITER ',' CSV HEADER NULL 'N/A';
+\copy raw_staging_games FROM 'path/to/coding_projects/video_game_sales/vgsales.csv' DELIMITER ',' CSV HEADER NULL 'N/A';
+\copy staging_games FROM 'path/to/coding_projects/video_game_sales/vgsales.csv' DELIMITER ',' CSV HEADER NULL 'N/A';
 -- NULL values in the CSV are represented as 'N/A', 
 --so we specify that in the COPY command to ensure they are correctly interpreted as NULL in the database.
 --Noticed as went to load CSV but data type of year column is INT but there are some 'N/A' values which caused error.
